0ee379a9adc2b3d2069d4df536d96741804b30d5
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-04-30 07:12:00 -0500

    comp. flow

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ function getUserChoice(userInput) {
     userInput = userInput.toLowerCase();
 
     if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
-        console.log(`${userInput}`);
+        console.log(`User Input: ${userInput}`);
     } else {
         console.log(`Invalid input`);
     }
     
     // console.log(userInput)
 };
-getUserChoice('nice');
+getUserChoice('rock');
 
 function getComputerChoice() {
     //gets the comp. input
@@ -28,3 +28,26 @@ function getComputerChoice() {
 
 console.log("This is the fnc getting called" + " " + getComputerChoice());
 
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {    //!Tie game
+        console.log(`Tie game. Better luck next time.`)
+    } else if(userChoice === "rock"){       //!Rock
+        if (computerChoice === "paper") {
+            console.log(`Computer has WON!   -line 36`)
+        }
+    } else if(userChoice === "paper") {     //!Paper
+        if (computerChoice === "rock" || computerChoice === "scissors") {
+            console.log(`Computer has LOST!  -line 42`);
+        } else {
+            console.log(`User has WON!   -line 44`);
+        }
+    } else if (userChoice === "scissors") {     //!Scissors
+        if (computerChoice === "rock" || computerChoice === "paper") {
+            console.log(`Comp. Wins   -line 48`);
+        } else {
+            console.log(`Users Wins   -line 50`);
+        }
+    }
+}
+
+determineWinner("paper", "scissors");
