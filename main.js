421ee896381cf46f46c1e26fca47067357d38e5c
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-04-29 21:03:10 -0500

    Compt. bot response done.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function getUserChoice(userInput) {
+    //Gets user Inputs
     userInput = userInput.toLowerCase();
 
     if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
@@ -7,7 +8,23 @@ function getUserChoice(userInput) {
         console.log(`Invalid input`);
     }
     
-    console.log(userInput)
+    // console.log(userInput)
 };
-
 getUserChoice('nice');
+
+function getComputerChoice() {
+    //gets the comp. input
+    let randomNumGen = Math.floor(Math.random() * 3);
+
+    if (randomNumGen === 0) {
+        console.log(`rock`)
+    } else if (randomNumGen === 1) {
+        console.log(`paper`)
+    } else {
+        console.log(`scissors`)
+    }
+    return randomNumGen
+}
+
+console.log("This is the fnc getting called" + " " + getComputerChoice());
+
